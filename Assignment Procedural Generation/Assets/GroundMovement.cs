acfec0174bfb5ce040160e6899f1bfa24ea449bf
jakebolger <c18395341@mytudublin.ie>
2021-11-16 13:44:26 +0000

    spawner cube

---
+++ Assignment Procedural Generation/Assets/GroundMovement.cs
@@ -0,0 +1,70 @@
+
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+
+    public int depth = 20;
+
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+
+    void Start()
+    {
+        offsetX = Random.Range(0f, 9999f);
+        offsetY = Random.Range(0f, 9999f);
+    }
+    void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+        offsetX += Time.deltaTime * 5f;
+    }
+
+
+    // offsetX += Time.deltaTime * 5f;
+    // Start is called before the first frame update
+    
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        
+
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0, 0, GenerateHeights());
+
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+
+
+}

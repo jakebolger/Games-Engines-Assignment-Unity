5834693e9af9693f52a9ab92e9799e0b2f297db7
jakebolger <c18395341@mytudublin.ie>
2021-12-06 12:17:20 +0000

    rotation Changes

--- Assignment Procedural Generation/Assets/MiniGame/Scripts/OrbittingScript.cs
+++ Assignment Procedural Generation/Assets/MiniGame/Scripts/OrbittingScript.cs
@@ -4,36 +4,63 @@ using UnityEngine;
 
 public class OrbittingScript : MonoBehaviour
 {
+    //varibles for distance we want to move on the x and z axis
+    //
     public float xDistance;
     public float zDistance;
 
+    //gives a way to have the camera above or below our centerpoint.
+    //
     public float yOffset;
+    
+    //centerpoint reference varible.
+    //
     public Transform centerPoint;
 
+    //speed and direction variables when rotating.
+    //
     public float rotationSpeed;
     public bool rotateDirection;
 
+    //timer to update our position. always incrementing.
+    //
     float timer = 0;
 
 
 
-    // Update is called once per frame
+    // Update method that updates the movement.
+    //
     void Update()
     {
+        //increment the timer.
+        //
         timer += Time.deltaTime * rotationSpeed;
+        
+        //calling the Rotate().
+        //
         Rotate();
+        
+        //make transform of the camera look ast the centerpoint.
+        //
         transform.LookAt(centerPoint);
     }
 
     void Rotate()
     {
+        //check if we want to rotate clockwise or counter clockwise.
+        //
         if (rotateDirection)
         {
+            //getting new float that is the equivalent of -COs * time. allows it to repeat infinitely.
+            //
             float x = -Mathf.Cos(timer) * xDistance;
             float z = Mathf.Sin(timer) * zDistance;
 
+            //creates vector3 for posiiton.
+            //
             Vector3 pos = new Vector3(x, yOffset, z);
 
+            //
             transform.position = pos + centerPoint.position;
         }
         else

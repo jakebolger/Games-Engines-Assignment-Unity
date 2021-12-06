d35f490053d4b29c3979f24898a823b9cee3be3f
jakebolger <c18395341@mytudublin.ie>
2021-12-06 12:03:28 +0000

    marble Changes

--- Assignment Procedural Generation/Assets/MiniGame/Scripts/MarbleMove.cs
+++ Assignment Procedural Generation/Assets/MiniGame/Scripts/MarbleMove.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class MarbleMove : MonoBehaviour
 {
-
+    //variable for movement speed.
+    //
     public float speed = 5.0f;
+    
+    //varibale for horizontal input.
+    //
     private float horizontalInput;
+    
+    //variable for forward input.
+    //
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        //sets Horizontalinput from project settings to  "Horizontal".
+        //
         horizontalInput = Input.GetAxis("Horizontal");
+        //sets forwardinput from project settings to  "Vertical".
+        //
         forwardInput = Input.GetAxis("Vertical");
 
+        //uses two vector3s to allow movement using the variables.
+        //
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }

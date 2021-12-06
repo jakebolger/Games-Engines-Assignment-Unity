bd683d6c79c53e6ec8c53e54058fb18cedd24bf6
jakebolger <c18395341@mytudublin.ie>
2021-12-06 12:23:05 +0000

    collision

--- Assignment Procedural Generation/Assets/MiniGame/Scripts/PlayerSpawn.cs
+++ Assignment Procedural Generation/Assets/MiniGame/Scripts/PlayerSpawn.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
-
+    //serializes script components, creating variables for the player and the respawn point so game objects can be used.
+    //
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
+    //method that uses OnTriggerEnter so that when the marblePlayer collides with the trigger it respawns at the respawn point.
+    //
     void OnTriggerEnter(Collider other)
     {
         player.transform.position = respawnPoint.transform.position;
